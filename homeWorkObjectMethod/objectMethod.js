405374cfa9c33147502a87bea1d10785e5993cbc
rafgrig <grigoryanrafael515@gmail.com>
2024-09-04 19:39:01 +0400

    write a func that return lowest score in given array of objects

---
+++ homeWorkObjectMethod/objectMethod.js
@@ -0,0 +1,78 @@
+const evaluations = [
+    {
+    studentId: 1, // Unique identifier for each student
+    studentName: "John Doe",
+    courseId: 1, // Unique identifier for each course
+    courseName: "JS Bootcamp",
+    score: 70
+    },
+    {
+    studentId: 1, // Unique identifier for each student
+    studentName: "John Doe",
+    courseId: 2, // Unique identifier for each course
+    courseName: "JS Level 0",
+    score: 60
+    },
+    {
+    studentId: 1, // Unique identifier for each student
+    studentName: "John Doe",
+    courseId: 3, // Unique identifier for each course
+    courseName: "JS Level 1",
+    score: 50
+    },
+    {
+    studentId: 2, // Unique identifier for each student
+    studentName: "Tom Colins",
+    courseId: 1, // Unique identifier for each course
+    courseName: "JS Bootcamp",
+    
+    score: 60
+    },
+    {
+    studentId: 2, // Unique identifier for each student
+    studentName: "Tom Colins",
+    courseId: 2, // Unique identifier for each course
+    courseName: "JS Level 0",
+    score: 50
+    },
+    {
+    studentId: 2, // Unique identifier for each student
+    studentName: "Tom Colins",
+    courseId: 3, // Unique identifier for each course
+    courseName: "JS Level 1",
+    score: 40
+    },
+    {
+    studentId: 3, // Unique identifier for each student
+    studentName: "Sarah Conor",
+    courseId: 1, // Unique identifier for each course
+    courseName: "JS Bootcamp",
+    score: 45
+    },
+    {
+    studentId: 3, // Unique identifier for each student
+    studentName: "Sarah Conor",
+    courseId: 2, // Unique identifier for each course
+    courseName: "JS Level 0",
+    score: 35
+    },
+    {
+    studentId: 3, // Unique identifier for each student
+    studentName: "Sarah Conor",
+    courseId: 3, // Unique identifier for each course
+    
+    courseName: "JS Level 1",
+    score: 25
+    }
+]
+
+function getLowestScore(arr=[]){
+    const lowestScore = arr.reduce((acc, item) => {
+        return item.score < acc ? item.score : acc;
+    }, arr[0].score);
+    
+    return lowestScore
+}
+
+console.log(getLowestScore(evaluations))
+
