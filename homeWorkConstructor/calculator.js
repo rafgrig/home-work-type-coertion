d4bcab409a1fc71979c8c55f45b824da84f072cb
rafgrig <grigoryanrafael515@gmail.com>
2024-09-30 20:26:06 +0400

    write constructor functon that has add, subtract, multiply, dive methods

---
+++ homeWorkConstructor/calculator.js
@@ -0,0 +1,23 @@
+function Calculator(){
+}
+
+Calculator.prototype={
+    add(firstNumber, secondNumber){
+        firstNumber + secondNumber
+    },
+    sumtract(firstNumber, secondNumber){
+        firstNumber - secondNumber
+    },
+    multiply(firstNumber, secondNumber){
+        firstNumber * secondNumber
+    },
+    divide(firstNumber, secondNumber){
+        firstNumber / secondNumber
+    }
+}
+
+const calculator = new Calculator();
+calculator.add(10, 5);
+calculator.subtract(10, 5);
+calculator.multiply(10, 5);
+calculator.divide(10, 5);
