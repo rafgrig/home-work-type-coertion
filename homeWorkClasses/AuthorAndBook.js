89309ae7f37fb79ee3e73104958960b38d383c9f
rafgrig <grigoryanrafael515@gmail.com>
2024-10-11 18:45:35 +0400

    wite book constructor that has title author, price, quantity. every parametr has it's own getter and setter.every parametr's setter, but price's and quantity's, check is given argument is a string, if it is it's change, if not, didn't change.

--- homeWorkClasses/AuthorAndBook.js
+++ homeWorkClasses/AuthorAndBook.js
@@ -31,3 +31,39 @@ class Author{
         return `Author's name is ${this.name}. Author's email is ${this.email}`
     }
 }
+
+class Book {
+    constructor(title="", author="", price=0, quantity=0){
+        this.title = title
+        this.author = author
+        this.price = price
+        this.quantity = quantity
+    }
+    get title(){
+        return this.title
+    }
+    set title(title){
+        if(typeof title === "string") this.title = title
+    }
+
+    get author(){
+        return this.author
+    }
+    set author(author){
+        if(typeof author === "string") this.author = author
+    }
+
+    get price(){
+        return this.price
+    }
+    set price(price){
+        if(typeof price === "number" && !isNaN(price)) this.price = price
+    }
+
+    get quantity(){
+        return this.quantity
+    }
+    set quantity(quantity){
+        if(typeof quantity === "number" && !isNaN(quantity)) this.quantity = quantity
+    }
+}
